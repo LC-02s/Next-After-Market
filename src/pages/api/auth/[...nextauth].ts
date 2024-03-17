a455e65c01c2493ec49ed97cbc8b86d895bad747
ChanLee_KR <cl700120@gmail.com>
2024-03-17 22:01:24 +0900

    add : 데이터베이스 연결 및 next-auth 추가

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,31 @@
+import NextAuth, { NextAuthOptions } from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+import CredentialsProvider from "next-auth/providers/credentials"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        username: { label: 'Username', type: 'text', placeholder: 'jsmith' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials, req) {
+        const user = { id: '1', name: 'J Smith', email: 'jsmith@example.com' }
+        return user ? user : null;
+      }
+    })
+  ],
+  session: { strategy: 'jwt' }
+}
+
+export default NextAuth(authOptions);
